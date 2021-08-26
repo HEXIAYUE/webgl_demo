af8923e7b6e7284706a1aad7ae7c0cf232df744b
hexiayue <1506589520@qq.com>
2021-08-27 01:20:20 +0800

    配置所需库和开发规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'for-direction': 'error',
+		'no-unreachable': 2, // 在return，throw，continue，break语句后不允许出现不可能到达的语句
+		'use-isnan': 2, // 要求检查NaN的时候使用isNaN()
+		quotes: ['error', 'single', { allowTemplateLiterals: true }], // 单引号 允许模板文字,
+	},
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import * as THREE from 'three'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-Vue.config.productionTip = false;
+// Vue.prototype.$Three = THREE
+// Vue.prototype.$OrbitControls = OrbitControls
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount('#app')
