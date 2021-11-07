044de707f172855befa92973911b5e513d944e19
hexiayue <1506589520@qq.com>
2021-11-08 01:18:07 +0800

    解决vue-router报错的bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
+// 解决导航栏或者底部导航tabBar中的vue-router在3.0版本以上频繁点击菜单报错的问题。
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
 Vue.use(VueRouter);
 
 const routes = [
